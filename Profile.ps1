50785d3e0ba7abce6bec6579606cdac0d59b28bb
shenchupeng <shenchupeng@corp.netease.com>
2020-08-19 10:14:43 +0800

    update comment

--- Profile.ps1
+++ Profile.ps1
@@ -1,7 +1,7 @@
 # enable fzf integration
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
 
-# enable vcvars64 for native dev
+# enable vcvars64 for native development
 cmd.exe /c "call `"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat`" && set > %temp%\vcvars.txt"
 Get-Content "$env:temp\vcvars.txt" | Foreach-Object {
   if ($_ -match "^(.*?)=(.*)$") {
